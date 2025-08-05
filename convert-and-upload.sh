40a8175d5db33e04601d8e9f8e0dbff9d62574d5
Elvis Mao <info@elvismao.com>
2025-08-05 13:55:19 +0800

    feat: image

--- convert-and-upload.sh
+++ convert-and-upload.sh
@@ -6,11 +6,16 @@ OUT="output.jpg"
 
 # Progress calculation
 TODAY=$(TZ=Asia/Taipei date +%s)
-TARGET=$(date -d "$TARGET_DATE" +%s)
-TOTAL=$(( (TARGET - START_DATE) / 86400 ))
-LEFT=$(( (TARGET - TODAY) / 86400 ))
+START_TIMESTAMP=$(date -jf "%Y-%m-%d" "$TELEGRAM_START_DATE" +%s)
+TARGET_TIMESTAMP=$(date -jf "%Y-%m-%d" "$TELEGRAM_TARGET_DATE" +%s)
+
+# Compute days
+TOTAL=$(( (TARGET_TIMESTAMP - START_TIMESTAMP) / 86400 ))
+LEFT=$(( (TARGET_TIMESTAMP - TODAY) / 86400 ))
 DAYS_DONE=$(( TOTAL - LEFT ))
 PERCENT=$(( 100 * DAYS_DONE / TOTAL ))
+
+echo "Progress: $DAYS_DONE/$TOTAL days ($PERCENT%)"
 
 # Image size
 HEIGHT=$(identify -format "%h" "$IMG")
@@ -20,17 +25,23 @@ WIDTH=$(identify -format "%w" "$IMG")
 COLOR_HEIGHT=$(( HEIGHT * PERCENT / 100 ))
 
 # Process image
-convert "$IMG" \
+magick "$IMG" \
   -gravity North -crop "${WIDTH}x$(( HEIGHT - COLOR_HEIGHT ))+0+0" +repage \
   -colorspace Gray "top.jpg"
 
-convert "$IMG" \
+magick "$IMG" \
   -gravity South -crop "${WIDTH}x${COLOR_HEIGHT}+0+0" +repage \
   "bottom.jpg"
 
-convert top.jpg bottom.jpg -append "$OUT"
+magick top.jpg bottom.jpg -append "$OUT"
 
-# Upload
-curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/setChatPhoto" \
-  -F chat_id="$CHAT_ID" \
-  -F photo=@"$OUT"
+TITLE="SITCON 2026 工人大群 | 倒數 $LEFT 天"
+
+# curl -s -X POST https://api.telegram.org/bot${{ secrets.TELEGRAM_BOT_TOKEN }}/setChatTitle \
+#   -d chat_id=${{ secrets.TELEGRAM_CHAT_ID }} \
+#   -d title="$TITLE"
+
+# # Upload
+# curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/setChatPhoto" \
+#   -F chat_id="$CHAT_ID" \
+#   -F photo=@"$OUT"
